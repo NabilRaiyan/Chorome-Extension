37c8f8a3fb545203f57c85cede8c756c59712a5d
NabilRaiyan <rraiyan77@gmail.com>
2024-08-25 21:51:26 +0600

    creating chrome extension using react

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -6,6 +6,38 @@ import { useState } from 'react'
 function App() {
   const [color, setColor] = useState('');
 
+  const getUrl = async () => {
+    const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+    chrome.scripting.executeScript({
+      target: {tabId: tab.id!},
+      args: [tab],
+      func: (tab) => {
+        const url = tab.url;
+        try{
+          const queryString = url?.split('?')[1];
+          const searchParam = new URLSearchParams(queryString);
+          const searchString = searchParam.get('q');
+          const searchWords = searchString ? searchString.split('%20'): [];
+          console.log(searchWords)
+          if(searchWords === null || searchWords.length === 0){
+            const query = url?.split('/');
+            const lastWord = query ? query[query.length - 1] : '';
+            console.log(lastWord);
+          }
+        }
+        catch(e){
+          console.log(e)
+        }
+        
+      }
+    });
+  };
+  
+  // Call the function
+  getUrl();
+  
+  
+
   const alertFunc = async ()=> {
     const [tab] = await chrome.tabs.query({active: true});
     chrome.scripting.executeScript<string[], void>({
@@ -32,6 +64,7 @@ function App() {
 
         <input type='color' onChange={(e) => setColor(e.currentTarget.value) }></input>
         <button onClick={alertFunc}>Click me</button>
+        <button onClick={getUrl}>Get Url</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
