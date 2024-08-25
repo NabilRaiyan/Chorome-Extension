b138876880b61c67a15a9596ccb8ce0d7a12ed2e
NabilRaiyan <rraiyan77@gmail.com>
2024-08-25 20:10:57 +0600

    creating chrome extension using react

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState();
+
+  const alertFunc = async ()=> {
+    const [tab] = await chrome.tabs.query({active: true});
+    chrome.scripting.executeScript({
+      target: {tabId: tab.id!},
+      func: ()=>{
+        document.body.style.backgroundColor = 'red';
+      }
+    })
+  }
 
   return (
     <>
@@ -18,9 +28,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+
+        <input></input>
+        <button onClick={alertFunc}>Click me</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
